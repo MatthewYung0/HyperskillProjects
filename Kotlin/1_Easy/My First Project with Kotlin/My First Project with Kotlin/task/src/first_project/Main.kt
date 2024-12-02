23a790b9f1f7fb81b7bc96c69ce81ca827312202
myung <myung@volvocars.com>
2024-12-02 09:29:10 +0100

    Stage 1 - My First Project with Kotlin

---
+++ Kotlin/1_Easy/My First Project with Kotlin/My First Project with Kotlin/task/src/first_project/Main.kt
@@ -0,0 +1,18 @@
+package first_project
+
+fun main() {
+    val bubblegum = 2
+    val toffee = 0.2
+    val iceCream = 5
+    val milkChocolate = 4
+    val doughnut = 2.5
+    val pancake = 3.2
+
+    println("Prices: ")
+    println("Bubblegum: $$bubblegum")
+    println("Toffee: $$toffee")
+    println("Ice cream: $$iceCream")
+    println("Milk chocolate: $$milkChocolate")
+    println("Doughnut: $$doughnut")
+    println("Pancake: $$pancake")
+}
